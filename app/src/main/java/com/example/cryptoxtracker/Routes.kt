9269e2569bd7772d2491fcddd96e5bdefcfbff7a
iam-aniket <aniketgray@gmail.com>
2024-11-21 18:25:51 +0530

    Added Routes Object

---
+++ app/src/main/java/com/example/cryptoxtracker/Routes.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoxtracker
+
+object Routes {
+    var homeScreen = "home_screen"
+    var cryptoScreen = "crypto_portfolio"
+    var stockScreenIND = "stock_portfolio_IND"
+    var stockScreenUSA = "stock_portfolio_USA"
+    var otherAssetScreen = "other_portfolio"
+    var detailScreen = "detail_screen"
+}
