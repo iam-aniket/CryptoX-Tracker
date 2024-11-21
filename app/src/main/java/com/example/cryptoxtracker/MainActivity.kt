25183b8eb2e2065bec0647031d9123137e6de90f
iam-aniket <aniketgray@gmail.com>
2024-11-21 18:17:25 +0530

    Added MVVM Folder Structure

--- app/src/main/java/com/example/cryptoxtracker/MainActivity.kt
+++ app/src/main/java/com/example/cryptoxtracker/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.cryptoxtracker.model.CryptoValues
 import com.example.cryptoxtracker.ui.theme.CryptoXTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +14,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CryptoXTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                /*Scaffold(modifier = Modifier.fillMaxSize()) {
+
+                }*/
+                Text(CryptoValues.cryptoQuantitiesMapFake.toString())
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoXTrackerTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/cryptoxtracker/model/CryptoValues.kt
@@ -0,0 +1,35 @@
+package com.example.cryptoxtracker.model
+
+object CryptoValues {
+    val cryptoQuantitiesMapFake: Map<String, Double> = mapOf(
+        "bitcoin" to 1.00,
+        "ethereum" to 2.00,
+        "tether" to 3.00,
+        "binancecoin" to 4.00,
+        "solana" to 5.00,
+        "dogecoin" to 6.00,
+        "tron" to 7.00,
+        "avalanche-2" to 8.00,
+        "chainlink" to 9.00,
+        "polkadot" to 10.00,
+        "near" to 11.00,
+        "aptos" to 12.00,
+        "pepe" to 13.00,
+        "fetch-ai" to 14.00,
+        "polygon-ecosystem-token" to 15.00,
+        "dogwifcoin" to 16.00,
+        "optimism" to 17.00,
+        "render-token" to 18.00,
+        "arbitrum" to 19.00,
+        "cosmos" to 20.00,
+        "floki" to 21.00,
+        "worldcoin-wld" to 22.00,
+        "bittorrent" to 23.00,
+        "ecash" to 24.00,
+        "book-of-meme" to 25.0,
+        "1inch" to 26.0,
+        "harmony" to 27.0,
+        "jupiter-exchange-solana" to 28.0,
+        "cats" to 29.0
+    )
+}
