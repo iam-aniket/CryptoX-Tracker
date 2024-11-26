701018171c1c811ca6f5a090ab455a0170541219
iam-aniket <aniketgray@gmail.com>
2024-11-26 23:26:09 +0530

    Added comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.13.0")  // Latest version of Moshi
-    implementation("com.squareup.moshi:moshi-kotlin:1.13.0") // For Kotlin support
+    implementation("com.squareup.moshi:moshi:1.13.0")  //Latest version of Moshi
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0") //For Kotlin support
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("io.coil-kt:coil-compose:2.2.0")
